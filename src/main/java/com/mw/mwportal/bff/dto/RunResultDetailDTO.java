5fb4f0698272193995924ced3d6862661ab087f8
meditch05 <meditch05@gmail.com>
2020-08-05 14:31:58 +0900

    Remove lombok

--- src/main/java/com/mw/mwportal/bff/dto/RunResultDetailDTO.java
+++ src/main/java/com/mw/mwportal/bff/dto/RunResultDetailDTO.java
@@ -1,16 +1,21 @@
 package com.mw.mwportal.bff.dto;
 
-import lombok.Getter;
+//import lombok.Getter;
 
 import com.google.gson.annotations.SerializedName;
 
-@Getter
+// @Getter
 public class RunResultDetailDTO {
 
     private String empno;
     private String salary;
     private String fromdate;
     private String todate;
+    
+    public String getEmpno()	{ return empno; }
+    public String getSalary()	{ return salary; }
+    public String getFromdate() { return fromdate; }
+    public String getTodate()	{ return todate; }
 
     @Override
     public String toString() {
